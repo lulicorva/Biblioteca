4ebca49709af99f505265ff8cee6beb54c758a66
Lucila <luli.c-@hotmail.com>
2020-03-20 13:41:49 -0300

    Parte E

--- src/com/company/Libro.java
+++ src/com/company/Libro.java
@@ -1,14 +1,27 @@
 package com.company;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Libro {
     private String nombre;
     private Integer codigoISBN;
     private String autor;
+    private List<Ejemplar>listaDeEjemplares;
 
     public Libro(String nombre, Integer codigoISBN, String autor){
         this.nombre=nombre;
         this.codigoISBN=codigoISBN;
         this.autor=autor;
+        this.listaDeEjemplares= new ArrayList<>();
+    }
+
+    public void setListaDeEjemplares(List<Ejemplar> listaDeEjemplares) {
+        this.listaDeEjemplares = listaDeEjemplares;
+    }
+
+    public List<Ejemplar> getListaDeEjemplares() {
+        return listaDeEjemplares;
     }
 
     public void setNombre(String nombre) {
